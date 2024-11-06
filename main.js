9153aaa577d341572131196755aa113676bf0bcb
Neemias <163009399+alkatrauz@users.noreply.github.com>
2024-11-06 19:34:35 -0300

    att2

--- main.js
+++
@@ -1,8 +0,0 @@
-let formulario = document.getElementById('form-cadastro');
-let tbody = document.querySelector('#tabela-cadastros tbody');
-formulario.querySelector('button').onclick = function() {
-  let nome = formulario.nome.value;
-  let email = formulario.email.value;
-  tbody.innerHTML += `<tr><td>${nome}</td><td>${email}</td></tr>`;
-  formulario.reset();
-};
